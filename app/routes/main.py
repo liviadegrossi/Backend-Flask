142663ec9e13695e57c32c4a0feb7880f0b15113
Livia Degrossi <liviadegrossi@gmail.com>
2026-03-07 11:04:47 +0100

    Route to validate the user login

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,8 @@
 # Blueprint organizes all the routes
 # jsonify converts dictionaries into JSON
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from pydantic import ValidationError # to validate our models using pydantic
+from app.models.user import LoginPayload
 
 main_bp = Blueprint('main_bp', __name__)
 
@@ -8,7 +10,23 @@ main_bp = Blueprint('main_bp', __name__)
 # HTTP POST METHOD
 @main_bp.route('/login', methods=['POST'])
 def login():
-    return jsonify({'message': 'Route to login'})
+    try:
+        raw_data = request.get_json() # reads the body (JSON) and returns a dictionary
+        # the ** operator transforms the dictionary (raw_data) into data for the attributes
+        user_login = LoginPayload(**raw_data)
+
+    except ValidationError as error:
+        return jsonify({'error': error.errors()}), 400 # http status_code
+    except Exception as e: # for any other inconsistency
+        return jsonify({'error': 'Error in the request'}), 500
+    
+    if user_login['username'] == 'admin' and user_login['password'] == '12345':
+        return jsonify({'message': 'Login successful!'})
+    else:
+        return jsonify({'message': 'Wrong username or password'})
+    
+    # model_dump_json transforms the dictionary back to a json
+    # return jsonify({'message': f'Route to login the user {user_login.model_dump_json()}'})
 
 # RF: O sistema deve permitir listagem de todos os produtos
 # HTTP GET METHOD (default)
@@ -44,6 +62,3 @@ def upload_sales():
 @main_bp.route('/')
 def index():
     return jsonify({'message': 'Welcome to the StyleSync!'})
-
-
-
